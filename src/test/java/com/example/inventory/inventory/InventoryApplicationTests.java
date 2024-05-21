ea80c07c15b15b949d379d1034f9c5530d5d3b6b
Karan Sachdev <sachdev.karan3@outlook.com>
2024-05-21 15:15:40 +0530

    first commit

---
+++ src/test/java/com/example/inventory/inventory/InventoryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.inventory.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class InventoryApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
