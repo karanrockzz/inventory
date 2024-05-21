9996916e7fd83238880297e3ea5104b0a2dffc64
Karan Sachdev <sachdev.karan3@outlook.com>
2024-05-21 16:33:58 +0530

    Eureka Client added

--- src/main/java/com/example/inventory/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/inventory/InventoryApplication.java
@@ -1,32 +1,23 @@
 package com.example.inventory.inventory;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.discovery.EurekaClient;
 
 
 @RestController
 @SpringBootApplication
 public class InventoryApplication {
 
- 	@Autowired
-    @Lazy
-    private EurekaClient eurekaClient; 
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(InventoryApplication.class).web(WebApplicationType.SERVLET).run(args);
+		new SpringApplicationBuilder(InventoryApplication.class).run(args);
 	}
 
 	@RequestMapping("/greeting")
     public String greeting() {
         return String.format(
-          "Hello World!",eurekaClient.getApplication(null);
+          "Hello World!");
     }
 
 }
